291d919c78c6f6c777dd687bbf6c66d30265b93e
Pär Eriksson <pe222gq@student.lnu.se>
2017-03-25 13:41:49 +0100

    Adding module foundations.

---
+++ index.js
@@ -0,0 +1,8 @@
+const game = require('./shroomcoon-core')
+
+// game.start()
+// game.deltaTime
+// game.on('event').then().catch() // update, fixedUpdate, tick, pause, resume, customEvent...
+// game.emit('event')
+// game.draw()
+// game.objectPool
